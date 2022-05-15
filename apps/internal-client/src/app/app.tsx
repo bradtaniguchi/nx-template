3f46b92ab140af1695c721d2c928c9873e2f00ce
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-05-15 13:32:02 -0700

    Add hello world

--- apps/internal-client/src/app/app.tsx
+++ apps/internal-client/src/app/app.tsx
@@ -1,11 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="internal-client" />
+      <h1>Hello world!</h1>
       <div />
     </>
   );
