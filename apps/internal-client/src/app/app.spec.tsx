5a651c4723216a18b6eb61d7662b18ec873d7014
Brad <bradtaniguchi@gmail.com>
2022-05-19 17:31:57 -0700

    chore/debug ci (#2)
    - Add new on-push workflow that checks which projects are affected for multiple targets. This is simpler than the previous `nx-workspace-template` approach, which attempts to generalize all tasks.

--- apps/internal-client/src/app/app.spec.tsx
+++ apps/internal-client/src/app/app.spec.tsx
@@ -12,6 +12,6 @@ describe('App', () => {
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
 
-    expect(getByText(/Welcome internal-client/gi)).toBeTruthy();
+    expect(getByText(/Hello World!/gi)).toBeTruthy();
   });
 });
