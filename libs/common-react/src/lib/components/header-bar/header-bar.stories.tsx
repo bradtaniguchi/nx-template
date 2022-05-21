9932cf5cb1f465a2567c08f4cc673b6541f626f6
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-05-21 12:08:29 -0700

    Update minor settings for common-react header
    - will be the first component, next is a basic sidenav

--- libs/common-react/src/lib/components/header-bar/header-bar.stories.tsx
+++ libs/common-react/src/lib/components/header-bar/header-bar.stories.tsx
@@ -1,5 +1,9 @@
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 import { Story, Meta } from '@storybook/react';
-import { HeaderBar, HeaderBarProps } from './header-bar';
+import { HeaderBar, HeaderBarProps, HeaderBarContext } from './header-bar';
 
 export default {
   component: HeaderBar,
@@ -27,6 +31,36 @@ Empty.args = {
 
 export const AuthenticatedEmptyExample = Template.bind({});
 AuthenticatedEmptyExample.args = {};
+AuthenticatedEmptyExample.decorators = [
+  (Story) => (
+    <HeaderBarContext.Provider
+      value={
+        // This is an example of an authenticated "toolbar"
+        <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'row' }}>
+          <Typography
+            component="div"
+            sx={{
+              flexGrow: 1,
+            }}
+          >
+            {/* I can't center this vertically! */}
+            <div>Authenticated Example</div>
+          </Typography>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="Authenticated User Icon"
+          >
+            <AccountCircle />
+          </IconButton>
+        </Box>
+      }
+    >
+      <Story />
+    </HeaderBarContext.Provider>
+  ),
+];
 
 export const WithSearchExample = Template.bind({});
 WithSearchExample.args = {};
