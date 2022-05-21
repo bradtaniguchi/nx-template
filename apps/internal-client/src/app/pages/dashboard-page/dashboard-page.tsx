51655970d5fc9080692d6ad788c7b9c6ffb664e1
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-05-21 13:35:12 -0700

    Add useConfig hook.

--- apps/internal-client/src/app/pages/dashboard-page/dashboard-page.tsx
+++ apps/internal-client/src/app/pages/dashboard-page/dashboard-page.tsx
@@ -1,12 +1,29 @@
+import { BaseConfig, useConfig } from '@nx-template/common-react';
+import { useMemo } from 'react';
 import styles from './dashboard-page.module.scss';
 
 /* eslint-disable-next-line */
 export interface DashboardPageProps {}
 
 export function DashboardPage(props: DashboardPageProps) {
+  const { config, configError, configLoading } = useConfig({
+    overrides: useMemo(
+      () =>
+        ({
+          tag: 'local',
+          ref_type: 'tag',
+          sha: '',
+        } as Partial<BaseConfig>),
+      []
+    ),
+  });
+
   return (
     <div className={styles['container']}>
       <h1>Welcome to DashboardPage!</h1>
+      {configLoading && <div>Loading config...</div>}
+      {!!configError && <div>Error Loading Config</div>}
+      {config && <div>{JSON.stringify(config)}</div>}
     </div>
   );
 }
--- libs/common-react/src/index.ts
+++ libs/common-react/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/components/header-bar/header-bar';
+export * from './lib/hooks';
 export * from './lib/types';
 export * from './lib/utils';
