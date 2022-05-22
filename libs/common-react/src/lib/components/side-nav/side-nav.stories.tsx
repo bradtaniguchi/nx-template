b52f71e0a53dce977dd5f781184f096dcb3e015d
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-05-22 11:24:07 -0700

    Add side-nav basic stories

--- libs/common-react/src/lib/components/side-nav/side-nav.stories.tsx
+++ libs/common-react/src/lib/components/side-nav/side-nav.stories.tsx
@@ -1,19 +1,28 @@
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 import { Meta, Story } from '@storybook/react';
 import { SideNav, SideNavProps } from './side-nav';
 export default {
   component: SideNav,
-  title: 'HeaderBar',
+  title: 'SideNav',
   argTypes: {
-    onHamburgerClick: {
-      action: 'onHamburgerClick',
-    },
-    onMenuClick: {
-      action: 'onMenuClick',
+    onClose: {
+      action: 'onClose',
     },
   } as Record<keyof SideNavProps, unknown>,
 } as Meta;
 
 const Template: Story<SideNavProps> = (args) => <SideNav {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {};
+export const BasicTextList = Template.bind({});
+BasicTextList.args = {
+  opened: true,
+  items: ['Item 1', 'Item 2'].map((text) => (
+    <ListItem key={text} disablePadding>
+      <ListItemButton>
+        <ListItemText primary={text} />
+      </ListItemButton>
+    </ListItem>
+  )),
+};
--- libs/common-react/src/lib/components/side-nav/side-nav.tsx
+++ libs/common-react/src/lib/components/side-nav/side-nav.tsx
@@ -1,13 +1,49 @@
-import styles from './side-nav.module.scss';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import Drawer from '@mui/material/Drawer';
+import { ReactNode, SyntheticEvent } from 'react';
 
 /* eslint-disable-next-line */
-export interface SideNavProps {}
+export interface SideNavProps {
+  /**
+   * The side the side-nav is to be displayed on.
+   * Top is also an option, but this will not be used.
+   *
+   * @default 'left'
+   */
+  anchor?: 'left' | 'right' | 'bottom';
+  /**
+   * If the side-nav is opened
+   */
+  opened?: boolean;
+  /**
+   * Array of items to be displayed within the `List`
+   * of the nav
+   */
+  items?: ReactNode[];
+  /**
+   * When the side-nav emits the close event.
+   */
+  onClose?: (
+    event: SyntheticEvent,
+    reason: 'backdropClick' | 'escapeKeyDown'
+  ) => void;
+}
 
 export function SideNav(props: SideNavProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to SideNav!</h1>
-    </div>
+    <Drawer
+      anchor={props.anchor ?? 'left'}
+      open={props.opened}
+      onClose={props.onClose}
+    >
+      <Box
+        sx={{ width: props.anchor === 'bottom' ? 'auto' : 250 }}
+        role="presentation"
+      >
+        <List>{props.items || []}</List>
+      </Box>
+    </Drawer>
   );
 }
 
