5543792c5b52b39627a42934d73d4d7dd7f2267c
Brad <bradtaniguchi@gmail.com>
2022-06-03 08:11:37 -0700

    Add combine components helper function (#15)
    Close #14

---
+++ libs/common-react/src/lib/utils/combine-components.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { ComponentProps, FC } from 'react';
+
+/**
+ * Combines components together, from the given array.
+ * Usually is used to "flatten" highly nested structures, such as
+ * multiple context providers.
+ */
+export const combineComponents = (...components: Array<FC<any>>): FC => {
+  return components.reduce(
+    (AccumulatedComponents, CurrentComponent) => {
+      return ({ children }: ComponentProps<FC<any>>): JSX.Element => {
+        return (
+          <AccumulatedComponents>
+            <CurrentComponent>{children}</CurrentComponent>
+          </AccumulatedComponents>
+        );
+      };
+    },
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    ({ children }) => <>{children}</>
+  );
+};
