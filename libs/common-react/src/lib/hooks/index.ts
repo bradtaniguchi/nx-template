be42f5018bb513421da3dcd1c84b3f4c9c7a130f
Brad <bradtaniguchi@gmail.com>
2022-07-02 11:15:26 -0700

    Add use-script hook (#37)

--- libs/common-react/src/lib/hooks/index.ts
+++ libs/common-react/src/lib/hooks/index.ts
@@ -2,3 +2,4 @@ export * from './use-config';
 export * from './use-local-forage';
 export * from './use-logger';
 export * from './use-nx-graph';
+export * from './use-script';
---
+++ libs/common-react/src/lib/hooks/use-script.ts
@@ -0,0 +1,50 @@
+// largely taken from:
+// https://stackoverflow.com/questions/34424845/adding-script-tag-to-react-jsx
+import { useEffect } from 'react';
+
+/**
+ * Options that could be passed to the userScript hook.
+ * @unstable
+ */
+export interface UseScriptOptions {
+  /**
+   * The url of the script to load.
+   */
+  url: string;
+  /**
+   * If the script should be loaded as async,
+   * this defaults to true.
+   */
+  async?: boolean;
+  /**
+   * Callback for the `onload` event of the script.
+   * Can be used to
+   */
+  onLoad?: () => void;
+}
+
+/**
+ * Hook that adds a script to the page.
+ *
+ * This should be called only 1 time
+ * @unstable
+ */
+export function useScript(options?: UseScriptOptions) {
+  useEffect(() => {
+    const { url, async, onLoad } = options || {};
+
+    if (!url) return;
+
+    const script = document.createElement('script');
+
+    script.src = url;
+    script.async = async ?? true;
+    if (typeof onLoad === 'function') script.onload = onLoad;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [options]);
+}
