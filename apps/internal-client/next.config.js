1d2c55fb9b0a137e33b4a54b1d0db940e88a90d6
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-07-27 20:49:57 -0700

    Add sentry config

--- apps/internal-client/next.config.js
+++ apps/internal-client/next.config.js
@@ -1,5 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+const { withSentryConfig } = require('@sentry/nextjs');
 const isProd = process.env.NODE_ENV === 'production';
 
 /**
@@ -14,4 +19,16 @@ const nextConfig = {
   assetPrefix: isProd ? '/nx-template/' : '',
 };
 
-module.exports = withNx(nextConfig);
+module.exports = withNx(
+  withSentryConfig(nextConfig, {
+    // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, org, project, authToken, configFile, stripPrefix,
+    //   urlPrefix, include, ignore
+
+    silent: true, // Suppresses all logs
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
+  })
+);
