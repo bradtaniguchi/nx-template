cfa8c87ad1608c2d8a182141cb0f5a362cdd9687
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-11-26 13:22:43 -0800

    chore: fix failing tests for header-bar

--- libs/common-react/src/lib/components/header-bar/header-bar.spec.tsx
+++ libs/common-react/src/lib/components/header-bar/header-bar.spec.tsx
@@ -1,9 +1,9 @@
 import { render } from '@testing-library/react';
-
-import HeaderBar from './header-bar';
+import { HeaderBar } from './header-bar';
 
 describe('HeaderBar', () => {
   it('should render successfully', () => {
+    // eslint-disable-next-line react/jsx-no-undef
     const { baseElement } = render(<HeaderBar />);
     expect(baseElement).toBeTruthy();
   });
