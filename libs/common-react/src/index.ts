5da7e32470612180f082d29276a1307f14a46c68
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-11-26 13:23:13 -0800

    chore: remove side-nav component
    - this component isn't used, will need to make a new version using
    tailwind.

--- libs/common-react/src/index.ts
+++ libs/common-react/src/index.ts
@@ -3,7 +3,6 @@ export * from './lib/components/button-link';
 export * from './lib/components/card';
 export * from './lib/components/google-analytics/google-analytics';
 export * from './lib/components/header-bar/header-bar';
-export * from './lib/components/side-nav/side-nav';
 export * from './lib/hooks';
 export * from './lib/types';
 export * from './lib/utils';
