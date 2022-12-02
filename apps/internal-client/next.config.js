c76bf34ab9f21f49d6ffdc04850bb04f2e1ce7a1
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-12-02 10:09:04 -0800

    fix: attempt fix at static asset path

--- apps/internal-client/next.config.js
+++ apps/internal-client/next.config.js
@@ -7,29 +7,31 @@ const withNx = require('@nrwl/next/plugins/with-nx');
 const { withSentryConfig } = require('@sentry/nextjs');
 const isProd = process.env.NODE_ENV === 'production';
 
-/**
- * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
- */
 const nextConfig = {
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
-  basePath: isProd ? '/nx-template/' : '',
-  assetPrefix: isProd ? '/nx-template/' : '',
 };
 
-module.exports = withNx(
-  withSentryConfig(nextConfig, {
-    // Additional config options for the Sentry Webpack plugin. Keep in mind that
-    // the following options are set automatically, and overriding them is not
-    // recommended:
-    //   release, url, org, project, authToken, configFile, stripPrefix,
-    //   urlPrefix, include, ignore
+/**
+ * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
+ */
+module.exports = {
+  ...withNx(
+    withSentryConfig(nextConfig, {
+      // Additional config options for the Sentry Webpack plugin. Keep in mind that
+      // the following options are set automatically, and overriding them is not
+      // recommended:
+      //   release, url, org, project, authToken, configFile, stripPrefix,
+      //   urlPrefix, include, ignore
 
-    silent: true, // Suppresses all logs
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options.
-  })
-);
+      silent: true, // Suppresses all logs
+      // For all available options, see:
+      // https://github.com/getsentry/sentry-webpack-plugin#options.
+    })
+  ),
+  // Needs to be overwritten here, as either withNx or withSentryConfig overwrites it.
+  assetPrefix: isProd ? '/nx-template' : '',
+};
