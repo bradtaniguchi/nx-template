1cc5989655281daa48c4d2a98902312087662007
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-12-03 13:43:01 -0800

    chore: fixed overflow of config
    - should fix overflow issue seen on mobile.

--- apps/internal-client/components/dashboard-page/dashboard-page-config.tsx
+++ apps/internal-client/components/dashboard-page/dashboard-page-config.tsx
@@ -12,7 +12,7 @@ export const DashboardPageConfig = memo(function DashboardPageConfig({
       <h5 className="text-lg dark:text-white">config.json</h5>
       <div>
         <div className="rounded outline outline-gray-400">
-          <pre className="m-0 dark:bg-slate-800 dark:text-white">
+          <pre className="m-0 overflow-x-auto dark:bg-slate-800 dark:text-white">
             {JSON.stringify(config, null, 2)}
           </pre>
         </div>
