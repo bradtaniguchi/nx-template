abdf50bde0cc8a6ce106f845f42649b70bf91e8a
Brad <bradtaniguchi@gmail.com>
2022-12-03 15:17:15 -0800

    chore/110/optimize props (#114)
    * chore: optimize props for index page
    
    - only returns what the client-side needs to display.
    
    * chore: update prefixes for local
    
    - removes warning
    
    * chore: update init-job version
    
    * chore: downgrade version to 2
    
    * chore: revert downgrade
    
    - need to migrate the actual flow to a PR file, will be a downgrade but
    help limit CI runs.
    
    * chore: revert version upgrade for init-job

--- apps/internal-client/components/dashboard-page/dashboard-page-projects.tsx
+++ apps/internal-client/components/dashboard-page/dashboard-page-projects.tsx
@@ -1,26 +1,18 @@
-import { getProjectsByTarget } from '@nx-template/common-react';
 import { Card } from 'flowbite-react';
-import { ProjectGraph } from 'nx/src/config/project-graph';
-import { memo, useMemo } from 'react';
 import Link from 'next/link';
+import { memo } from 'react';
 
 export const DashboardPageProjects = memo(function DashboardPageProjects({
-  nxGraph,
+  typedocProjects,
 }: {
-  nxGraph: ProjectGraph<unknown>;
+  typedocProjects: string[];
 }) {
-  const projects = useMemo(
-    () =>
-      nxGraph ? getProjectsByTarget({ graph: nxGraph, target: 'typedoc' }) : [],
-    [nxGraph]
-  );
-
   return (
     <Card>
       <h5 className="text-lg dark:text-white">Generated TSDocs</h5>
       <div>
         <ul className="grid grid-cols-1 gap-2 sm:grid-cols-2">
-          {projects.map((project) => (
+          {typedocProjects.map((project) => (
             <li key={project}>
               <div className="flex flex-row align-middle dark:text-white">
                 {/* <ArticleIcon color="primary" /> */}
