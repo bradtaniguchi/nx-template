2adffd386f6962b2ea2d8eaf0db95f692435cab3
Brad Taniguchi <bradtaniguchi@gmail.com>
2023-03-04 14:16:14 -0800

    feat: add gen-typedoc generator
    - part of plugins library

---
+++ libs/plugins/src/generators/gen-typedoc/generator.ts
@@ -0,0 +1,84 @@
+import {
+  addProjectConfiguration,
+  formatFiles,
+  generateFiles,
+  getWorkspaceLayout,
+  names,
+  offsetFromRoot,
+  Tree,
+} from '@nrwl/devkit';
+import * as path from 'path';
+import { GenTypedocGeneratorSchema } from './schema';
+
+interface NormalizedSchema extends GenTypedocGeneratorSchema {
+  projectName: string;
+  projectRoot: string;
+  projectDirectory: string;
+  parsedTags: string[];
+}
+
+/**
+ *
+ * @param tree
+ * @param options
+ */
+function normalizeOptions(tree: Tree, options: GenTypedocGeneratorSchema): NormalizedSchema {
+  const name = names(options.name).fileName;
+  const projectDirectory = options.directory
+    ? `${names(options.directory).fileName}/${name}`
+    : name;
+  const projectName = projectDirectory.replace(new RegExp('/', 'g'), '-');
+  const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${projectDirectory}`;
+  const parsedTags = options.tags
+    ? options.tags.split(',').map((s) => s.trim())
+    : [];
+
+  return {
+    ...options,
+    projectName,
+    projectRoot,
+    projectDirectory,
+    parsedTags,
+  };
+}
+
+/**
+ *
+ * @param tree
+ * @param options
+ */
+function addFiles(tree: Tree, options: NormalizedSchema) {
+    const templateOptions = {
+      ...options,
+      ...names(options.name),
+      offsetFromRoot: offsetFromRoot(options.projectRoot),
+      template: ''
+    };
+    generateFiles(tree, path.join(__dirname, 'files'), options.projectRoot, templateOptions);
+}
+
+/**
+ *
+ * @param tree
+ * @param options
+ */
+export default async function (tree: Tree, options: GenTypedocGeneratorSchema) {
+  const normalizedOptions = normalizeOptions(tree, options);
+  addProjectConfiguration(
+    tree,
+    normalizedOptions.projectName,
+    {
+      root: normalizedOptions.projectRoot,
+      projectType: 'library',
+      sourceRoot: `${normalizedOptions.projectRoot}/src`,
+      targets: {
+        build: {
+          executor: "@nx-template/plugins:build",
+        },
+      },
+      tags: normalizedOptions.parsedTags,
+    }
+  );
+  addFiles(tree, normalizedOptions);
+  await formatFiles(tree);
+}
---
+++ libs/plugins/src/generators/gen-typedoc/schema.d.ts
@@ -0,0 +1,5 @@
+export interface GenTypedocGeneratorSchema {
+    name: string;
+    tags?: string;
+    directory?: string;
+}
